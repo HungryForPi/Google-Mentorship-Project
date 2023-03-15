5e087e3b3904a0726fa11f79278d086a521eab55
ambers7 <ambrshen@gmail.com>
2023-03-15 09:10:10 -0400

    Load dataset , error file loading not supported

--- mediapipe/demo.py
+++ mediapipe/demo.py
@@ -1,4 +1,4 @@
-# from google.colab import files
+# from google.colab import files (WHY DOES THIS NOT WORK :(( )))
 import os
 import tensorflow as tf
 assert tf.__version__.startswith('2')
@@ -16,7 +16,9 @@ for i in os.listdir(dataset_path):
   if os.path.isdir(os.path.join(dataset_path, i)):
     labels.append(i)
 print(labels)
-
+ 
+# SHOW EXAMPLES
+'''
 NUM_EXAMPLES = 5
 
 for label in labels:
@@ -30,3 +32,23 @@ for label in labels:
   fig.suptitle(f'Showing {NUM_EXAMPLES} examples for {label}')
 
 plt.show()
+'''
+
+# LOAD DATASET
+data = gesture_recognizer.Dataset.from_folder(
+    dirname=dataset_path,
+    hparams=gesture_recognizer.HandDataPreprocessingParams()
+)
+train_data, rest_data = data.split(0.8)
+validation_data, test_data = rest_data.split(0.5)
+
+hparams = gesture_recognizer.HParams(export_dir="exported_model")
+options = gesture_recognizer.GestureRecognizerOptions(hparams=hparams)
+model = gesture_recognizer.GestureRecognizer.create(
+    train_data=train_data,
+    validation_data=validation_data,
+    options=options
+)
+
+loss, acc = model.evaluate(test_data, batch_size=1)
+print(f"Test loss:{loss}, Test accuracy:{acc}")
