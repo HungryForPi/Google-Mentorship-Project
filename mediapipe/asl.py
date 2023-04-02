ba91e5166ebb60c299053628224bc8949c52f189
ambers7 <ambrshen@gmail.com>
2023-04-01 21:06:45 -0400

    mediapipe - rename nothing folder to none

--- mediapipe/asl.py
+++ mediapipe/asl.py
@@ -5,8 +5,10 @@ assert tf.__version__.startswith('2')
 
 from mediapipe_model_maker import gesture_recognizer
 
+# import matplotlib
+# matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-dataset_path = "asl_alphabet_train/asl_alphabet_train"
+dataset_path = "mediapipe/asl_alphabet_train/asl_alphabet_train"
 
 print(dataset_path)
 labels = []
@@ -15,29 +17,31 @@ for i in os.listdir(dataset_path):
     labels.append(i)
 print(labels)
  
-# SHOW EXAMPLES
+# # SHOW EXAMPLES
 
-NUM_EXAMPLES = 5
+# NUM_EXAMPLES = 5
 
-for label in labels:
-  label_dir = os.path.join(dataset_path, label)
-  example_filenames = os.listdir(label_dir)[:NUM_EXAMPLES]
-  fig, axs = plt.subplots(1, NUM_EXAMPLES, figsize=(10,2))
-  for i in range(NUM_EXAMPLES):
-    axs[i].imshow(plt.imread(os.path.join(label_dir, example_filenames[i])))
-    axs[i].get_xaxis().set_visible(False)
-    axs[i].get_yaxis().set_visible(False)
-  fig.suptitle(f'Showing {NUM_EXAMPLES} examples for {label}')
+# for label in labels:
+#   label_dir = os.path.join(dataset_path, label)
+#   example_filenames = os.listdir(label_dir)[:NUM_EXAMPLES]
+#   fig, axs = plt.subplots(1, NUM_EXAMPLES, figsize=(10,2))
+#   for i in range(NUM_EXAMPLES):
+#     axs[i].imshow(plt.imread(os.path.join(label_dir, example_filenames[i])))
+#     axs[i].get_xaxis().set_visible(False)
+#     axs[i].get_yaxis().set_visible(False)
+#   fig.suptitle(f'Showing {NUM_EXAMPLES} examples for {label}')
+#   plt.show()
+#   plt.close()
 
-plt.show()
-
+# # plt.show()
+# # plt.close()
 
 # LOAD DATASET
 data = gesture_recognizer.Dataset.from_folder(
     dirname=dataset_path,
     hparams=gesture_recognizer.HandDataPreprocessingParams()
 )
-train_data, rest_data = data.split(0.8)
+train_data, rest_data = data.split(.1)
 validation_data, test_data = rest_data.split(0.5)
 
 hparams = gesture_recognizer.HParams(export_dir="exported_model")
