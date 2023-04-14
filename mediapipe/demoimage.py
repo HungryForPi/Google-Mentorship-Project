2b11c521de939c4e5b51e7e782bf37a163364c76
ambers7 <ambrshen@gmail.com>
2023-04-14 17:45:19 -0400

    mediapipe - recognize image but it doesnt work

--- mediapipe/demoimage.py
+++ mediapipe/demoimage.py
@@ -2,30 +2,29 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python.components import processors
 from mediapipe.tasks.python import vision
+import cv2
+import math
+import sys
 
-BaseOptions = mp.tasks.BaseOptions
-ImageClassifier = mp.tasks.vision.ImageClassifier
-ImageClassifierOptions = mp.tasks.vision.ImageClassifierOptions
-VisionRunningMode = mp.tasks.vision.RunningMode
-# STEP 2: Create an ImageClassifier object.
-base_options = python.BaseOptions(model_asset_path='exported_model/demo_gesture_recognizer.task')
-options = vision.ImageClassifierOptions(
-    base_options=base_options, max_results=4)
-classifier = vision.ImageClassifier.create_from_options(options)
-
-images = []
-predictions = []
-# for image_name in IMAGE_FILENAMES:
+# BaseOptions = mp.tasks.BaseOptions
+# ImageClassifier = mp.tasks.vision.ImageClassifier
+# ImageClassifierOptions = mp.tasks.vision.ImageClassifierOptions
+# VisionRunningMode = mp.tasks.vision.RunningMode
+base_options = python.BaseOptions(model_asset_path='exported_model/rps_gesture_recognizer.task')
+options = vision.GestureRecognizerOptions(base_options=base_options)
+recognizer = vision.GestureRecognizer.create_from_options(options)
 # STEP 3: Load the input image.
-image = mp.Image.create_from_file('asl_alphabet_test/asl_alphabet_test/A_test.jpg')
+image = mp.Image.create_from_file('mediapipe/asl_alphabet_test/asl_alphabet_test/A_test.jpg')
 
 # STEP 4: Classify the input image.
-classification_result = classifier.classify(image)
+recognition_result = recognizer.recognize(image)
 
 # STEP 5: Process the classification result. In this case, visualize it.
-images.append(image)
-top_category = classification_result.classifications[0].categories[0]
-predictions.append(f"{top_category.category_name} ({top_category.score:.2f})")
+# images.append(image)
+top_gesture = recognition_result.gestures
+print(top_gesture)
+# hand_landmarks = recognition_result.hand_landmarks
+# results.append((top_gesture, hand_landmarks))
 
-print(predictions)
+print(recognition_result)
 # display_batch_of_images(images, predictions) this is a google colab thing
