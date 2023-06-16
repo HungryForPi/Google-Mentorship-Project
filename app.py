56778eb31d665f1dadcbb8fb1bbe96a32aeecca0
Calvin Zhang <calvinzhang@calvins-air.mynetworksettings.com>
2023-06-16 13:33:45 -0400

    implemenet flask livestream by krishnaik06

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, Response
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
+
 import asl_recognizer
+import cv2
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
@@ -23,6 +25,30 @@ def home():
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
         return render_template('results.html', results = asl_recognizer.recognize_image(file.filename), filename = file.filename)
     return render_template('index.html', form=form)
+
+@app.route('/livestream')
+def livestream():
+    return render_template('livestream.html')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+camera=cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 if __name__ == '__main__':
     app.run(debug=True)
